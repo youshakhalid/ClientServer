e7751a50c81c6611941f9c7e1070b44e2e40f923
Yousha Bin Khalid <ybkhalid@trafix.com>
2025-02-07 16:56:55 +0500

    Add project files.

---
+++ ClientServer/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using ClientSide;
+
+ClientServer();
+
+
+
+static void ClientServer()
+{
+    List<Users> listUser = new List<Users>
+{
+    new Users {name="Yousha", designation="jr"},
+    new Users {name="Urwa", designation="Sr"}
+};
+
+    try
+    {
+        //Establishing and setting up Connection
+        IPHostEntry iPHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress iPAddress = iPHostEntry.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(iPAddress, 11111);
+
+        Socket sender = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            sender.Connect(localEndPoint);
+            Console.WriteLine($"Socket Connected to {sender.RemoteEndPoint.ToString()}");
+
+            //for sending messages
+
+            byte[] messageSent = ListToByte(listUser);
+            sender.Send(messageSent);
+            
+           
+
+            //for recieving message
+            byte[] messageRecieved = new byte[1024];
+            int byteRecieve = sender.Receive(messageRecieved);
+            var mStream = new MemoryStream(byteRecieve);
+            Console.WriteLine($"Recived Message: {Encoding.ASCII.GetString(messageRecieved, 0, byteRecieve)}");
+
+            sender.Shutdown(SocketShutdown.Both);
+            Console.ReadKey();
+            sender.Close();
+        }
+
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e);
+        }
+        catch (SocketException e) 
+        {
+            Console.WriteLine(e); 
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+static byte[] ListToByte(List<Users> users)
+{
+    MemoryStream memoryStream = new MemoryStream();
+    BinaryWriter writer = new BinaryWriter(memoryStream);
+    using (memoryStream)
+    {
+
+    }
+    using (writer)
+    {
+        writer.Write(users.Count);
+        foreach(var user in users)
+        {
+            writer.Write(user.name);
+            writer.Write(user.designation);
+
+        }
+        return memoryStream.ToArray();
+    }
+}
---
+++ ServerSide/Program.cs
@@ -0,0 +1,88 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using ClientSide;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+
+ExecuteServer();
+static void ExecuteServer()
+{
+
+    //Establishing and setting up Connection
+    IPHostEntry iPHostEntry = Dns.GetHostEntry(Dns.GetHostName());
+    IPAddress iPAddress = iPHostEntry.AddressList[0];
+    IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, 11111);
+
+    Socket listner = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+    try
+    {
+        listner.Bind(iPEndPoint);
+        listner.Listen(10);
+
+        while (true)
+        {
+            Console.WriteLine("Waiting for Connection...");
+            //Accept Client Connection
+            Socket clientSocket = listner.Accept();
+            Console.WriteLine("Connected:" + clientSocket.LocalEndPoint);
+            //Recieve Message
+            byte[] bytesCount = new byte[1024];
+            MemoryStream memoryStream = new MemoryStream();
+            int byteReceive;
+
+            while ((byteReceive = clientSocket.Receive(bytesCount)) > 0)
+            {
+                memoryStream.Write(bytesCount, 0, byteReceive);
+                if (byteReceive < bytesCount.Length)
+                    break;
+            }
+            // Console.WriteLine($"Message Recieved: {message}");
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            var users = ByteToList(memoryStream.ToArray());
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Name: {user.name}\nDesignation: {user.designation}\n------------------------------------");
+            }
+
+            //Send Message
+            byte[] sendMessage = Encoding.ASCII.GetBytes("Users Recieved");
+            clientSocket.Send(sendMessage);
+
+            //Shutdown Connection
+
+            clientSocket.Shutdown(SocketShutdown.Both);
+            Console.ReadKey();
+            clientSocket.Close();
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+
+    List<Users> ByteToList(byte[] arr)
+    {
+        var users = new List<Users>();
+        MemoryStream memoryStream = new MemoryStream(arr);
+        BinaryReader binaryReader = new BinaryReader(memoryStream);
+        using (memoryStream)
+        using (binaryReader)
+        {
+            int userCount = binaryReader.ReadInt32();
+            for (int i = 0; i < userCount; i++)
+            {
+                string name = binaryReader.ReadString();
+                string designation = binaryReader.ReadString();
+                users.Add(new Users { name = name, designation = designation });
+            }
+        }
+        return users;
+    }
+}
